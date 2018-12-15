9f18ed692dd3aab31e83ae5ec2fbdaab4044e8ed
quaffpotion <martino1729.gmail.com>
2018-12-14 18:03:14 -0600

    testing structuring

--- tensor.py
+++ tensor.py
@@ -1,27 +1,74 @@
 import numpy as np
 
+# Wrap a function so it keeps track of boxed arguments
+def primitive(func):
+    def primitive_func(*args, **kwargs):
+        if any(map(lambda x: type(x).__name__ not in ["wrapped_float","float"], args)):
+            parents = [x for x in args if (lambda x: type(x).__name__ not in ["wrapped_float","float"])(x)]
+            extracted_values = [x.value if (lambda x: type(x).__name__ not in ["wrapped_float","float"])(x) else x for x in args]
+            value = func(*extracted_values, **kwargs)
+            return Box(value, primitive_func, parents)
+        else:
+            return func(*args, **kwargs)
+    return primitive_func
+
+# Transforms a function f: floats -> float into a function 
+# f_wrapped: wrapped_floats -> wrapped_float provided the operations 
+# in f (e.g. +, *, etc.) have analogs for wrapped_floats
+def function_of_floats_wrapper(f):
+    def wrapped(*args, **kwargs):
+        wrapped_args = [wrapped_float(x) for x in args]
+        wrapped_kwargs = {key: wrapped_float(value) for key, value in kwargs.items()}
+        return f(*wrapped_args, **wrapped_kwargs)
+    return wrapped
+
+
 class Box():
     def __init__(self, value, func, parents):
         self.value = value
         self.func = func
         self.parents = parents
+        self.__add__ = primitive(value.__add__)
+        self.__mul__ = primitive(value.__mul__)
+        self.__radd__ = primitive(value.__radd__)
+        self.__rmul__ = primitive(value.__rmul__)
+
 
 
 class wrapped_float(float):
     def __new__(cls, value):
         return float.__new__(cls, value)
+
     def __add__(self, other):
         print("Adding!")
         return wrapped_float(float.__add__(self, other))
-    def __radd__(self, other):
-        print("Adding right")
-        return wrapped_float(float.__radd__(self, other))
+
     def __mul__(self, other):
         print("Multiplying!")
         return wrapped_float(float.__mul__(self, other))
-    def __rmul__(self, other):
-        print("Multiplying right!")
-        return wrapped_float(float.__r__(self, other))
+    
+
+x = wrapped_float(5.0)
+y = wrapped_float(6.0)
+z = wrapped_float(7.0)
+
+
+
+def f(x,y,z):
+    return y+z
+
+print(type(f(5.0, Box(6.0,None,[]), 7.0)))
+print(type(Box(5.0, None, [])))
+
+
+
+
+
+
+
+
+
+
 
 # This will play the same role for np.array as float.
 # This is the class the user is expected to use most often.
@@ -67,27 +114,6 @@ class Tensor(np.ndarray):
 # print((f+g).extra)
 
 
-# Transforms a function f: floats -> float into a function 
-# f_wrapped: wrapped_floats -> wrapped_float provided the operations 
-# in f (e.g. +, *, etc.) have analogs for wrapped_floats
-def function_of_floats_wrapper(f):
-    def wrapped(*args, **kwargs):
-        wrapped_args = [wrapped_float(x) for x in args]
-        wrapped_kwargs = {key: wrapped_float(value) for key, value in kwargs.items()}
-        return f(*wrapped_args, **wrapped_kwargs)
-    return wrapped
-
-# Wrap a function so it keeps track of boxed arguments
-def primitive(func):
-    def primitive_func(*args, **kwargs):
-        if any(map(lambda x: type(x).__name__ != "float", args)):
-            parents = [x for x in args if (lambda x: type(x).__name__ != "float")(x)]
-            extracted_values = [x.value if (lambda x: type(x).__name__ != "float")(x) else x for x in args]
-            value = func(*extracted_values, **kwargs)
-            return Box(value, primitive_func, parents)
-        else:
-            return func(*args, **kwargs)
-    return primitive_func
 
 @function_of_floats_wrapper
 def f(x,y):
@@ -95,12 +121,5 @@ def f(x,y):
     #a = 5 + z # todo: Here seems to use __rmul__ of int class
     a = z + 5
     return a
-
-x = wrapped_float(5)
-y = wrapped_float(6)
-x+ y
-x * y
-
-print(type(f(1,2)))
 
 
