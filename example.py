079388caff6af51f1647a6616dcadb60b5c2a3ef
quaffpotion <martino1729.gmail.com>
2018-12-25 23:09:51 -0600

    updates example.py

--- example.py
+++ example.py
@@ -14,6 +14,11 @@ print(grad(f,0)(2.0,3.0,4.0))
 print(grad(f,1)(2.0,3.0,4.0))
 print(grad(f,2)(2.0,3.0,4.0))
 
+for _ in toposort(f(Box(2.),Box(3.),Box(4.))):
+        print(_)
+
+print("end")
+
 
 
 def f(x,y,z):
@@ -30,7 +35,6 @@ def f(x,y,z):
 for _ in f(Box(2.0), Box(3.0), Box(4.0)):
     for __ in toposort(_):
         print(__)
-        
 
 # print(grad(f)(2.,3.))
 
@@ -40,6 +44,5 @@ for _ in f(Box(2.0), Box(3.0), Box(4.0)):
 # print((x+y).parents)
 # print((x+5.0).parents)
 # print((5.0+x).parents)
-
 
 # print(Box(6.9))
